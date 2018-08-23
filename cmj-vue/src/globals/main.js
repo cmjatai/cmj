79e6e273dfeb21f833079f20e76c5c47bd66ec04
Leandro Roberto <leandroroberto.br@gmail.com>
2018-08-23 09:17:50 -0300

    reorganiza estrutura de arquivos scss

--- cmj-vue/src/globals/main.js
+++ cmj-vue/src/globals/main.js
@@ -4,9 +4,10 @@ import 'jquery-ui-themes/themes/pepper-grinder/jquery-ui.min.css'
 
 import 'bootstrap'
 import 'jquery-mask-plugin'
+
 import 'webpack-jquery-ui/index'
 import 'webpack-jquery-ui/dialog'
 import 'webpack-jquery-ui/datepicker'
 import 'jquery-ui/ui/i18n/datepicker-pt-BR'
 
-import './app_cmj'
+import './2018/js/app_cmj'
