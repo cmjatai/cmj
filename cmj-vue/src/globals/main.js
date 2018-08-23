4023299edb747b9598dea9dec61a16c0a843df04
Leandro Roberto <leandroroberto.br@gmail.com>
2018-08-23 16:37:43 -0300

    migra tinymce para vue project

--- cmj-vue/src/globals/main.js
+++ cmj-vue/src/globals/main.js
@@ -5,9 +5,16 @@ import 'jquery-ui-themes/themes/pepper-grinder/jquery-ui.min.css'
 import 'bootstrap'
 import 'jquery-mask-plugin'
 
-import 'webpack-jquery-ui/index'
 import 'webpack-jquery-ui/dialog'
 import 'webpack-jquery-ui/datepicker'
+import 'webpack-jquery-ui/sortable'
 import 'jquery-ui/ui/i18n/datepicker-pt-BR'
+
+import 'tinymce/tinymce'
+
+import 'tinymce/themes/modern/theme'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/code'
+import 'tinymce/plugins/save'
 
 import './2018/js/app_cmj'
