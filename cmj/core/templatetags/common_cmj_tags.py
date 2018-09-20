9932e94ba8fce7efd3093fb6a801dcbee063fa01
Leandro Roberto <leandroroberto.br@gmail.com>
2018-09-20 13:27:16 -0300

    remove warning w3c sobre type javascript no webpack_loader

--- cmj/core/templatetags/common_cmj_tags.py
+++ cmj/core/templatetags/common_cmj_tags.py
@@ -7,9 +7,12 @@ from django.contrib.auth.tokens import default_token_generator
 from django.db.models.query import QuerySet
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from sapl.base.models import AppConfig
 from sapl.parlamentares.models import Filiacao
+from webpack_loader.templatetags.webpack_loader import render_bundle
+from webpack_loader.utils import _get_bundle
 
 
 register = template.Library()
@@ -207,3 +210,35 @@ def avatar_user(user_render=None):
     return {
         'user_render': user_render
     }
+
+
+def get_as_tags(bundle_name, extension=None, config='DEFAULT', attrs=''):
+    '''
+    Get a list of formatted <script> & <link> tags for the assets in the
+    named bundle.
+
+    :param bundle_name: The name of the bundle
+    :param extension: (optional) filter by extension, eg. 'js' or 'css'
+    :param config: (optional) the name of the configuration
+    :return: a list of formatted tags as strings
+    '''
+
+    bundle = _get_bundle(bundle_name, extension, config)
+    tags = []
+    for chunk in bundle:
+        if chunk['name'].endswith(('.js', '.js.gz')):
+            tags.append((
+                '<script src="{0}" {1}></script>'
+            ).format(chunk['url'], attrs))
+        elif chunk['name'].endswith(('.css', '.css.gz')):
+            tags.append((
+                '<link type="text/css" href="{0}" rel="stylesheet" {1}/>'
+            ).format(chunk['url'], attrs))
+    return tags
+
+
+@register.simple_tag
+def render_bundle(bundle_name, extension=None, config='DEFAULT', attrs=''):
+    tags = get_as_tags(bundle_name, extension=extension,
+                       config=config, attrs=attrs)
+    return mark_safe('\n'.join(tags))
