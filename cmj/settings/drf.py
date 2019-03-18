0f2114d2014294fabd04ebfe6f216111e2b0b91b
Leandro Roberto <leandroroberto.br@gmail.com>
2019-03-17 23:18:14 -0300

    altera classe de permissões e ajusta urls no frontend

--- cmj/settings/drf.py
+++ cmj/settings/drf.py
@@ -7,18 +7,20 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
-        # "rest_framework.renderers.BrowsableAPIRenderer",
+        # "rest_framework.renderers.BrowsableAPIRenderer", 
     ),
     "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
-        "sapl.api.permissions.DjangoModelPermissions",
+        "cmj.api.permissions.CmjModelPermissions",
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
     ),
-    "DEFAULT_PAGINATION_CLASS": "cmj.api.pagination.StandardPagination",
+    "DEFAULT_PAGINATION_CLASS": 
+        "cmj.api.pagination.StandardPagination",
     "DEFAULT_FILTER_BACKENDS": (
         "rest_framework.filters.SearchFilter",
         "django_filters.rest_framework.DjangoFilterBackend",
     ),
 }
+# "rest_framework.permissions.IsAuthenticated",
+        
