881d1b69afb2401b5621253541e4823de1fe3103
Leandro Roberto <leandroroberto.br@gmail.com>
2019-03-18 16:45:15 -0300

    add link offline para precache

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -71,10 +71,13 @@ const app = new Vue({ // eslint-disable-line
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      // console.log('SW registered: ', registration);
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError);
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        // console.log('SW registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+      
     });
-  });
 }
