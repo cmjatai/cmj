a3abef3d6ccee9878685d99a80f1973e9cf7d983
Leandro Roberto <leandroroberto.br@gmail.com>
2019-04-01 17:39:35 -0300

    corrige signal disconnect

--- cmj/s3_to_cmj/management/commands/s3sync.py
+++ cmj/s3_to_cmj/management/commands/s3sync.py
@@ -1,14 +1,8 @@
-from django.apps import apps
-from django.core.management.base import BaseCommand
-from django.db import connection
-from django.db.models.signals import post_save, post_delete
-from sapl.materia.models import MateriaLegislativa, DocumentoAcessorio
-from sapl.norma.models import NormaJuridica
-from sapl.parlamentares.models import Parlamentar
-from sapl.protocoloadm.models import DocumentoAdministrativo,\
-    DocumentoAcessorioAdministrativo
 
-from cmj.s3_to_cmj import mapa
+from django.db.models.signals import post_save, post_delete
+from sapl.rules.apps import sapl_post_delete_signal, sapl_post_save_signal
+
+from cmj.globalrules.apps import cmj_post_delete_signal, cmj_post_save_signal
 from cmj.s3_to_cmj.management.commands import s3import
 from cmj.s3_to_cmj.migracao_documentos_via_request import migrar_docs_por_ids
 
@@ -20,8 +14,10 @@ class Command(s3import.Command):
         parser.add_argument('sync', nargs='?', type=float, default=None)
 
     def handle(self, *args, **options):
-        post_delete.disconnect('sapl_post_delete_signal')
-        post_save.disconnect('sapl_post_save_signal')
+        post_delete.disconnect(sapl_post_delete_signal)
+        post_save.disconnect(sapl_post_save_signal)
+        post_delete.disconnect(cmj_post_delete_signal)
+        post_save.disconnect(cmj_post_save_signal)
 
         self.sync = options['sync']
         self.run()
