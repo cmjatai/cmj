b9b5885e3905a9c10e94d8eeb539c0bc9c751471
Leandro Roberto <leandroroberto.br@gmail.com>
2019-06-17 14:20:36 -0300

    add variável EMAIL_RUNNING

--- cmj/settings/email.py
+++ cmj/settings/email.py
@@ -11,3 +11,4 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', cast=str)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
 EMAIL_SEND_USER = config('EMAIL_SEND_USER', cast=str)
+EMAIL_RUNNING = None
