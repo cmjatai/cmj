4255a545c0ae52fb4a21cdd4733e3f592780da2d
Leandro Roberto <leandroroberto.br@gmail.com>
2019-08-21 13:26:26 -0300

    add menu sideright com normas de destaque

--- frontend/src/layouts/fragments/SideRight.vue
+++ frontend/src/layouts/fragments/SideRight.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="w-100 inner inner-sideright">
-    <ul>
-      <li>
-        <b-img @click="toogleNormaDestaque" src="@/assets/img/icon_normas_juridicas_destaque.png" fluid rounded="0" />
-        <ul :class="['list-group', 'isr-hover', menu_norma_destaque ? 'show': '']">
-          <li class="list-group-item"><a href="#">Teste 1</a></li>
-          <li class="list-group-item"><a href="#">Teste 2</a></li>
-          <li class="list-group-item"><a href="#">Teste 3</a></li>
-          <li class="list-group-item"><a href="#">Teste 4</a></li>
-        </ul>
-      </li>
-    </ul>
+    <div class="menu">
+      <ul>
+        <li>
+          <b-img @click="toogleNormaDestaque" src="@/assets/img/icon_normas_juridicas_destaque.png" fluid rounded="0" />
+          <ul class="list-group">
+            <li class="list-group-item"  v-for="item in itensNormasDeDestaque" :key="`srmd${item.id}`"><a href="#">{{item.apelido}}</a></li>
+          </ul>
+        </li>
+      </ul>
 
-    <!--b-img src="@/assets/img/icon_mesa_diretora.png" fluid rounded="0" />
-    <b-img src="@/assets/img/icon_comissoes.png" fluid rounded="0" />
-    <b-img src="@/assets/img/icon_parlamentares.png" fluid rounded="0" />
-    <b-img src="@/assets/img/icon_pautas.png" fluid rounded="0" />
-    <b-img src="@/assets/img/icon_plenarias.png" fluid rounded="0" />
-    <b-img src="@/assets/img/icon_materia_legislativa.png" fluid rounded="0" />
-    <b-img src="@/assets/img/icon_normas_juridicas.png" fluid rounded="0" />
-    <b-img src="@/assets/img/icon_relatorios.png" fluid rounded="0" /-->
+      <!--b-img src="@/assets/img/icon_mesa_diretora.png" fluid rounded="0" />
+      <b-img src="@/assets/img/icon_comissoes.png" fluid rounded="0" />
+      <b-img src="@/assets/img/icon_parlamentares.png" fluid rounded="0" />
+      <b-img src="@/assets/img/icon_pautas.png" fluid rounded="0" />
+      <b-img src="@/assets/img/icon_plenarias.png" fluid rounded="0" />
+      <b-img src="@/assets/img/icon_materia_legislativa.png" fluid rounded="0" />
+      <b-img src="@/assets/img/icon_normas_juridicas.png" fluid rounded="0" />
+      <b-img src="@/assets/img/icon_relatorios.png" fluid rounded="0" /-->
+    </div>
   </div>
 </template>
 
@@ -28,49 +27,96 @@ export default {
   name: 'side-right',
   data () {
     return {
-      mainProps: { class: 'icon' },
-      menu_norma_destaque: false
+      app: ['norma'],
+      model: ['normajuridica'],
+      menu_norma_destaque: false,
+      itens: {
+        normajuridica_list: {}
+      }
     }
+  },
+  computed: {
+    itensNormasDeDestaque: {
+      get () {
+        return _.orderBy(this.itens.normajuridica_list, 'apelido')
+      }
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.fetchModelListAction('norma', 'normajuridica', 'destaques', 1)
+    }, 1000)
   },
   methods: {
     toogleNormaDestaque (event) {
       this.menu_norma_destaque = !this.menu_norma_destaque
+    },
+
+    fetchItens (model_list = this.model) {
+      const t = this
+      _.mapKeys(model_list, function (value, key) {
+        _.mapKeys(t.itens[`${value}_list`], function (obj, k) {
+          obj.vue_validate = false
+        })
+        t.$nextTick()
+          .then(function () {
+            this.fetchModelListAction('norma', 'normajuridica', 'destaques', 1)
+          })
+      })
+    },
+
+    fetch (metadata) {
+      if (metadata.action === 'post_delete') {
+        this.$delete(this.itens[`${metadata.model}_list`], metadata.id)
+        return
+      }
+
+      const t = this
+      t.getObject(metadata)
+        .then(obj => {
+          t.$set(t.itens[`${metadata.model}_list`], metadata.id, obj)
+        })
     }
   }
 }
 </script>
 
 <style lang="scss">
-.inner-sideright {
+.inner-sideright .menu {
   //display: none;
-  & > ul {
+  a {
+    padding: 0.5rem 1rem;
+    white-space: nowrap;
+    display: block;
+    text-decoration: none;
+  }
+  ul {
     padding: 0;
     margin: 0;
     list-style-type: none;
-    & >li {
+    ul {
+     display: none;
+    }
+    li {
       position: relative;
-    }
-  }
-  ul {
-    &.isr-hover {
-      position: absolute;
-      right: 90%;
-      top: 50%;
-      display: none;
-      &.show {
-        display: flex;
-      }
-      a {
-        white-space: nowrap;
+      padding: 0;
+      &:hover {
+        background-color:  #ddd;
+        ul {
+          display: flex;
+          position: absolute;
+          right: 80%;
+          top: 50%;
+
+        }
       }
     }
   }
+
   img {
     padding: 15px;
     cursor: pointer;
-    &:hover {
-      background-color:  #ddd;
-    }
+
   }
 }
 </style>
