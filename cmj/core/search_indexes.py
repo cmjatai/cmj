1b1fc734c8f65c575ec5e2e2610b1573f344e8ac
Leandro Roberto <leandroroberto.br@gmail.com>
2019-08-26 11:09:18 -0300

    add __str__ no index de sessões

--- cmj/core/search_indexes.py
+++ cmj/core/search_indexes.py
@@ -1,17 +1,47 @@
 from celery_haystack.indexes import CelerySearchIndex
 from haystack.constants import Indexable
-from sapl.base.search_indexes import TextExtractField
+from haystack.fields import DateTimeField
+
+from sapl.base.search_indexes import TextExtractField as SaplTextExtractField
 from sapl.sessao.models import SessaoPlenaria
+
+
+class TextExtractField(SaplTextExtractField):
+
+    def extract_data(self, obj):
+
+        data = ''
+
+        for attr, func in self.model_attr:
+            if not hasattr(obj, attr) or not hasattr(self, func):
+                raise Exception
+
+            value = getattr(obj, attr)
+            if not value:
+                continue
+
+            if callable(value):
+                data += getattr(self, func)(value()) + '  '
+            else:
+                data += getattr(self, func)(value) + '  '
+
+        data = data.replace('\n', ' ')
+
+        return data
 
 
 class SessaoPlenariaIndex(CelerySearchIndex, Indexable):
     model = SessaoPlenaria
+    data = DateTimeField(model_attr='data_inicio', null=True)
     text = TextExtractField(
         document=True, use_template=True,
         model_attr=(
+            ('__str__', 'string_extractor'),
             ('upload_pauta', 'file_extractor'),
             ('upload_ata', 'file_extractor'),
             ('upload_anexo', 'file_extractor'),
+
+
         )
     )
 
