a8c96e726ba3e836768ce861e45af5ec60439647
Leandro Roberto <leandroroberto.br@gmail.com>
2019-08-27 18:14:09 -0300

    refatora form da busca textual

--- cmj/core/views_search.py
+++ cmj/core/views_search.py
@@ -1,13 +1,38 @@
 import re
 
+from crispy_forms.bootstrap import FieldWithButtons, StrictButton
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Div, Field, Layout
 from django.utils.translation import ugettext_lazy as _
 from haystack.forms import ModelSearchForm
 from haystack.views import SearchView
+
+from cmj.crispy_layout_mixin import to_row
+from sapl.crispy_layout_mixin import SaplFormLayout
 
 
 class CmjSearchForm(ModelSearchForm):
 
     def __init__(self, *args, **kwargs):
+
+        q_field = Div(
+            FieldWithButtons(
+                Field('q',
+                      placeholder=_('Busca Textual'),
+                      autocomplete='off',
+                      type='search',),
+                StrictButton(
+                    _('Pesquisar'), css_class='btn-outline-primary',
+                    type='submit')
+            )
+        )
+
+        row = to_row([(q_field, 8), (Div(Field('models')), 5)])
+
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(*row)
+
         super().__init__(*args, **kwargs)
 
         choices = self.fields['models'].choices
@@ -15,6 +40,7 @@ class CmjSearchForm(ModelSearchForm):
             if v[0] == 'sigad.documento':
                 choices[i] = (v[0], _('Notícias'))
         self.fields['models'].choices = sorted(choices, key=lambda x: x[1])
+        self.fields['models'].label = _('Buscar em conteúdos específicos')
 
     def search(self):
         sqs = super().search()
