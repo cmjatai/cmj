f901252fbfaf1ee726d296873f2e7ced2500865b
Leandro Roberto <leandroroberto.br@gmail.com>
2019-09-19 11:27:13 -0300

    add alert

--- cmj/legacy_sislegis_portal/management/commands/sportalimport_to_cmj.py
+++ cmj/legacy_sislegis_portal/management/commands/sportalimport_to_cmj.py
@@ -338,7 +338,7 @@ class Command(BaseCommand):
                         return value['item']['id_alterador']
             return 0
 
-        _ID = 0
+        _ID = 2222
 
         if _ID == 0:
             run_doc(_ID=_ID)
