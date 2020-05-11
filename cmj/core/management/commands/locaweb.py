f4ae262e2147bd6e649bd93a164606ea4451e190
Leandro Roberto <leandroroberto.br@gmail.com>
2020-05-11 11:15:50 -0300

    impl backup locaweb

--- cmj/core/management/commands/locaweb.py
+++ cmj/core/management/commands/locaweb.py
@@ -1,5 +1,5 @@
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 import os
 
@@ -13,7 +13,7 @@ from django.db.models.signals import post_delete, post_save
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
 
-from sapl.norma.models import NormaJuridica
+from sapl.utils import hash_sha512
 
 
 def _get_registration_key(model):
@@ -26,6 +26,9 @@ class Command(BaseCommand):
     s3r = None
 
     bucket_name = 'cmjatai_portal'
+    days_validate = 60
+
+    start_time = None
 
     def handle(self, *args, **options):
         post_delete.disconnect(dispatch_uid='sapl_post_delete_signal')
@@ -37,28 +40,52 @@ class Command(BaseCommand):
 
         self.s3_connect()
         # self.__clear_bucket('cmjatai_teste')
-        self.s3_sync(app_label='sapl.norma', model_name='NormaJuridica')
+        self.start_time = timezone.localtime()
+        self.s3_sync(model_name='MateriaLegislativa')
 
-    def download_file_from_object(self, bucket_name, obj):
+        print(self.start_time)
+        print(timezone.localtime())
+
+        #self.s3_sync(only_reset=False, model_name='NormaJuridica')
+        #self.s3_sync(app_label='sapl.norma', model_name='NormaJuridica')
+
+        #obj = n = NormaJuridica.objects.get(pk=8727)
+        #self.restore_file_from_object(self.bucket_name, obj)
+
+    def restore_file_from_object(self, bucket_name, obj):
         b = self.get_bucket(bucket_name)
-        obj = n = NormaJuridica.objects.get(pk=8711)
 
         for fn in obj.FIELDFILE_NAME:
 
             ff = getattr(obj, fn)
             if not ff:
                 continue
-            try:
-                b.download_file(
-                    ff.name, '/home/leandro/TEMP/teste_download_8711.pdf')
-            except Exception as e:
-                print(e)
+
+            directory = os.path.dirname(ff.path)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            if hasattr(ff, 'original_path'):
+                directory = os.path.dirname(ff.original_path)
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
 
             try:
                 b.download_file(
-                    ff.original_name, '/home/leandro/TEMP/{}'.format(ff.original_name))
+                    ff.name,
+                    ff.path
+                )
             except Exception as e:
                 print(e)
+
+            if hasattr(ff, 'original_path'):
+                try:
+                    b.download_file(
+                        ff.original_name,
+                        ff.original_path
+                    )
+                except Exception as e:
+                    print(e)
 
     def __clear_bucket(self, bucket_name):
         b = self.get_bucket(bucket_name)
@@ -101,7 +128,7 @@ class Command(BaseCommand):
         except:
             print('Erro na conexão com a locaweb')
 
-    def s3_sync(self, app_label=None, model_name=None):
+    def s3_sync(self, app_label=None, model_name=None, only_reset=False):
 
         print('--------- Locaweb ----------')
         reset = False
@@ -123,6 +150,14 @@ class Command(BaseCommand):
                     continue
 
                 for f in m._meta.get_fields():
+                    dua = f
+                    print(dua)
+                    if hasattr(dua, 'auto_now') and dua.auto_now:
+                        #print(m, 'auto_now deve ser desativado.')
+                        # continue  # auto_now deve ser desativado
+                        print(m, 'desativando auto_now')
+                        dua.auto_now = False
+
                     if not isinstance(f, FileField):
                         continue
 
@@ -141,12 +176,6 @@ class Command(BaseCommand):
                         f.name,
                         m._meta.label)
 
-                    dua = f
-                    if hasattr(dua, 'auto_now') and dua.auto_now:
-                        #print(m, 'auto_now deve ser desativado.')
-                        # continue  # auto_now deve ser desativado
-                        print(m, 'desativando auto_now')
-                        dua.auto_now = False
                     model_exec = True
 
                 if not model_exec:
@@ -157,13 +186,14 @@ class Command(BaseCommand):
                     if not hasattr(i, 'metadata'):
                         #print(i, 'não tem metadata')
                         continue
-                    # else:
-                    #    if i.metadata and \
-                    # 'locaweb' in i.metadata:
-                    #        i.metadata['locaweb'] = {}
-                    #        i.save()
-                    #        print(i)
-                    #    continue
+                    else:
+                        if only_reset:
+                            if i.metadata and \
+                                    'locaweb' in i.metadata:
+                                i.metadata['locaweb'] = {}
+                                i.save()
+                                print(i)
+                            continue
 
                     metadata = i.metadata if i.metadata else {}
                     for fn in i.FIELDFILE_NAME:
@@ -172,8 +202,9 @@ class Command(BaseCommand):
                         if not ff:
                             continue
 
-                        self.checar_consistencia(i, ff, fn)
-                        continue
+                        # if hasattr(ff, 'original_path'):
+                        #    self.checar_consistencia(i, ff, fn)
+                        # continue
 
                         if not metadata:
                             metadata = {}
@@ -184,22 +215,29 @@ class Command(BaseCommand):
                         if fn not in metadata['locaweb']:
                             metadata['locaweb'][fn] = {
                                 'path': None,
-                                'original_path': None
+                                'original_path': None,
+                                'validate': None,
+                                'hash': None,
+                                'original_hash': None
+
                             }
 
                         if reset:
                             metadata['locaweb'][fn] = {
                                 'path': None,
-                                'original_path': None
+                                'original_path': None,
+                                'validate': None,
+                                'hash': None,
+                                'original_hash': None
                             }
 
                         count_update = 0
                         try:
                             count_update += self.send_file(
-                                metadata, i, ff, fn, 'path')
+                                metadata, i, ff, fn, 'path', 'hash')
 
                             count_update += self.send_file(
-                                metadata, i, ff, fn, 'original_path')
+                                metadata, i, ff, fn, 'original_path', 'original_hash')
 
                         except Exception as e:
                             print(e)
@@ -209,34 +247,116 @@ class Command(BaseCommand):
                                 i.save()
                                 count += 1
 
-                            if count == 100:
+                            # se está em execução ha mais de 2min ou fez upload
+                            # de 100 registros
+                            if count == 100 or timezone.localtime() - self.start_time > timedelta(seconds=120):
                                 return
 
     def checar_consistencia(self, i, ff, fn):
-        existe_path = os.path.exists(ff.path)
-        existe_original_path = os.path.exists(ff.original_path)
-        if not existe_path:
-            print('ARQUIVO PATH NÃO ENCONTRADO:',
-                  i.id, i, ff.name)
-        if not existe_original_path:
-            print('ARQUIVO ORIGINAL PATH NÃO ENCONTRADO:',
-                  i.id, i, ff.name)
+        try:
+            existe_path = os.path.exists(ff.path)
+            existe_original_path = os.path.exists(ff.original_path)
+            if not existe_path:
+                print('ARQUIVO PATH NÃO ENCONTRADO:',
+                      i.id, i, ff.name)
+            if not existe_original_path:
+                dir_name = os.path.dirname(ff.original_path)
+                list_dir = os.listdir(dir_name)
+                if len(list_dir) == 1:
+                    file_name = os.path.basename(ff.path)
+                    original_file_rename_old = '{}/{}'.format(
+                        dir_name, list_dir[0])
+                    original_file_rename_new = '{}/{}'.format(
+                        dir_name, file_name)
 
-    def send_file(self, metadata, i, ff, fn, attr):
-        if os.path.exists(getattr(ff, attr)):
+                    os.rename(original_file_rename_old,
+                              original_file_rename_new)
+                else:
+                    print('ARQUIVO ORIGINAL PATH NÃO ENCONTRADO:',
+                          i.id, i, ff.name, len(ff.name))
+        except Exception as e:
+            print(i, e)
 
-            if metadata['locaweb'][fn][attr]:
-                t = os.path.getmtime(getattr(ff, attr))
-                date_file = datetime.fromtimestamp(
-                    t, timezone.utc)
+    def temp_file_from_object(self, bucket_name, obj, fn, attr_path):
 
-                if parse_datetime(metadata['locaweb'][fn][attr]) > date_file:
-                    return 0
+        ff = getattr(obj, fn)
+        if not ff:
+            return None
 
-            self.s3c.upload_file(
-                getattr(ff, attr),
+        if not hasattr(ff, attr_path):
+            return None
+
+        b = self.get_bucket(bucket_name)
+
+        t_p = '/tmp/br.leg.go.jatai.portalcmj.{}.{}.{}.{}.{}'.format(
+            attr_path,
+            obj._meta.app_label,
+            obj._meta.model_name,
+            obj.id,
+            fn
+        )
+
+        try:
+            r_p = b.download_file(
+                ff.name,
+                t_p
+            )
+
+            return t_p
+
+        except Exception as e:
+            print(e)
+            return False
+
+    def validate_file(self, metadata, i, fn, attr_path, attr_hash):
+        if metadata['locaweb'][fn]['validate']:
+            v = parse_datetime(metadata['locaweb'][fn]['validate'])
+
+            # if timezone.localtime() - v > timedelta(seconds=30):
+            if self.start_time - v < timedelta(days=self.days_validate):
+                return True
+
+            t_p = self.temp_file_from_object(
                 self.bucket_name,
-                ff.original_name if 'original' in attr else ff.name,
+                i,
+                fn, attr_path
+            )
+            if t_p is None:
+                return True
+            if not t_p:
+                return False
+
+            hash = hash_sha512(t_p)
+
+            if hash == metadata['locaweb'][fn][attr_hash]:
+                return True
+
+        else:
+            print('Documento Sem validação:', i.id, i)
+        return False
+
+    def send_file(self, metadata, i, ff, fn, attr_path, attr_hash):
+
+        if not hasattr(ff, attr_path):
+            return 0
+
+        if os.path.exists(getattr(ff, attr_path)):
+
+            if metadata['locaweb'][fn][attr_path]:
+
+                t = os.path.getmtime(getattr(ff, attr_path))
+                date_file = datetime.fromtimestamp(t, timezone.utc)
+
+                if parse_datetime(metadata['locaweb'][fn][attr_path]) > date_file:
+                    result = self.validate_file(
+                        metadata, i, fn, attr_path, attr_hash)
+                    if result:
+                        return 0
+            print('Enviando...', i, attr_path)
+            self.s3c.upload_file(
+                getattr(ff, attr_path),
+                self.bucket_name,
+                ff.original_name if 'original' in attr_path else ff.name,
                 ExtraArgs={
                     'ACL': 'private',
                     'Metadata': {
@@ -245,6 +365,10 @@ class Command(BaseCommand):
                 }
             )
 
-            metadata['locaweb'][fn][attr] = timezone.localtime()
+            metadata['locaweb'][fn][attr_path] = timezone.localtime()
+            metadata['locaweb'][fn][attr_hash] = hash_sha512(
+                getattr(ff, attr_path))
+            metadata['locaweb'][fn]['validate'] = timezone.localtime()
+
             return 1
         return 0
