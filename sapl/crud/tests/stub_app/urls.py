df7d6192492fd7747f77a0f8fa13c449b67efbbd
Leandro Roberto <leandroroberto.br@gmail.com>
2020-09-08 09:59:42 -0300

    remove stub crud

--- sapl/crud/tests/stub_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-
-from .views import CityCrud, CountryCrud
-
-urlpatterns = [
-    url(r'^country/', include(
-        CountryCrud.get_urls() + CityCrud.get_urls(), 'stub_app')),
-]
