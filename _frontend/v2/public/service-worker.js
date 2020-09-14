d8ecd2141aa218a579b7fea6e7cbf6b69b867141
Leandro Roberto <leandroroberto.br@gmail.com>
2020-09-14 10:36:54 -0300

    ajusta estrutura de imports de scss

--- _frontend/v2/public/service-worker.js
+++ _frontend/v2/public/service-worker.js
@@ -1,24 +1,20 @@
+/* eslint-disable no-undef */
 if (workbox) {
-  workbox.core.setCacheNameDetails({prefix: "frontend"})
+  workbox.core.setCacheNameDetails({ prefix: 'frontend' })
   self.__precacheManifest = [{
     url: '/offline/',
     revision: '000002'
   }].concat(self.__precacheManifest || [])
-  
-  workbox.precaching.suppressWarnings()
-  
+
   workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
-  //workbox.routing.registerRoute(
+  // workbox.routing.registerRoute(
   //  ({ event }) => event.request.mode === 'navigate', //if the requests is to go to a new url
   //  ({ url }) => fetch(url.href,{credentials: 'same-origin'}).catch(() => caches.match('/offline/')) //in case of not match send my to the offline page
-  //);
+  // );
 
   // console.log('self.__precacheManifest:')
   // console.log(self.__precacheManifest)
-
-} 
-else {
+} else {
   console.log(`Workbox didn't load`)
 }
-
