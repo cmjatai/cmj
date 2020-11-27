394c5ddacdc72b307dcd0fcb46ede3c8c4a7531d
Leandro Roberto <leandroroberto.br@gmail.com>
2020-11-27 08:40:53 -0300

    add all files

---
+++ _frontend/v1/dist/service-worker.js
@@ -0,0 +1,23 @@
+importScripts("/static/precache-manifest.cf230bacb9fe11f44738c1201d5486cc.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-undef */
+if (workbox) {
+  workbox.core.setCacheNameDetails({ prefix: 'frontend' })
+  self.__precacheManifest = [{
+    url: '/offline/',
+    revision: '000002'
+  }].concat(self.__precacheManifest || [])
+
+  workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+  // workbox.routing.registerRoute(
+  //  ({ event }) => event.request.mode === 'navigate', //if the requests is to go to a new url
+  //  ({ url }) => fetch(url.href,{credentials: 'same-origin'}).catch(() => caches.match('/offline/')) //in case of not match send my to the offline page
+  // );
+
+  // console.log('self.__precacheManifest:')
+  // console.log(self.__precacheManifest)
+} else {
+  // console.log(`Workbox didn't load`)
+}
+
