5dbd5aba5ee74586eb9786d25e6f1db00b0e85c5
Leandro Roberto <leandroroberto.br@gmail.com>
2021-11-03 09:12:42 -0300

    elimina pasta portal

--- _frontend/v3/src/main.js
+++ _frontend/v3/src/main.js
@@ -1,3 +1,23 @@
 import './registerServiceWorker'
+import { createApp } from 'vue'
+import BodyBaseDjangoTemplate from './root/BodyBaseDjangoTemplate'
+import VueNativeSock from 'vue-native-websocket-vue3'
+import router from './router'
+import store from './store'
 
-import './portal'
+import mixins from './mixins'
+
+let portal = createApp({
+  delimiters: ['[[', ']]'],
+  extends: {
+    ...BodyBaseDjangoTemplate
+  }
+}).use(store).use(router)
+
+portal = portal.use(VueNativeSock, (window.location.protocol === 'https:' ? 'wss://' : 'ws://') + window.location.host + '/ws/time-refresh/', {
+  connectManually: true
+})
+
+portal.mixin(mixins)
+
+portal.mount('#app-vue')
