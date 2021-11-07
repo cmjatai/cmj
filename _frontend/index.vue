6a374ad5068b285013166227c5eefeae9656b78b
Leandro Roberto <leandroroberto.br@gmail.com>
2021-11-06 23:46:54 -0300

    nuxt project com vuetify

---
+++ _frontend/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="max-w-screen-lg mx-auto max-w-screen-xl">
+    ------------------<br />
+    <br />
+    {{ rserver.__str__ }} <br />
+    {{ rserver.ementa }}
+    ------------------<br />
+    <br />
+    {{ rclient.__str__ }} <br />
+    {{ rclient.ementa }}
+  </div>
+</template>
+
+<script>
+export default {
+  fetchOnServer: false,
+  async asyncData({ params, $axios }) {
+    const rserver = await $axios.$get('/materia/materialegislativa/18600/')
+    return { rserver }
+  },
+  data() {
+    return {
+      rclient: {},
+    }
+  },
+  async fetch() {
+    this.rclient = await this.$axios.$get('/materia/materialegislativa/18500/')
+  },
+}
+</script>
--- _frontend/v3/plugins/wesocket.client.js
+++
@@ -1,7 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import VueNativeSock from 'vue-native-websocket-vue3'
-
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueNativeSock, 'wss://www.jatai.go.leg.br/ws/time-refresh/')
-})
