6884d1d7cafc2356d9276298b8d8869ef3500ea8
Leandro Roberto <leandroroberto.br@gmail.com>
2022-01-24 10:30:13 -0300

    personalização relatorios loa 2022

--- cmj/core/management/commands/rel_loa2022.py
+++ cmj/core/management/commands/rel_loa2022.py
@@ -4,7 +4,7 @@ import logging
 from PyPDF4.pdf import PdfFileReader, PdfFileWriter
 from django.core.management.base import BaseCommand
 from django.db.models.signals import post_delete, post_save
-from reportlab.lib.pagesizes import A4, landscape
+from reportlab.lib.pagesizes import A4, landscape, portrait
 from reportlab.lib.utils import open_and_read
 from reportlab.pdfgen import canvas
 import PyPDF4
@@ -27,56 +27,111 @@ class Command(BaseCommand):
         )
 
         self.run_autografo(
+            arquivos=self.get_parametros_846(),
+            epigrafe='AUTÓGRAFO Nº 846, DE 16 DE DEZEMBRO DE 2021.',
+            partes_ou_completo='C'
+        )
+
+        self.run_autografo(
             arquivos=self.get_parametros_847(),
             epigrafe='AUTÓGRAFO Nº 847, DE 16 DE DEZEMBRO DE 2021.',
             partes_ou_completo='C'
         )
 
+    def get_parametros_846(self):
+
+        coordenadas = [
+            (
+                0,
+                {
+                    'x': 57, 'y': 497, 'w': 92, 'h': 70,  # logotipo
+                    'xf': 55, 'yf': 493, 'wf': 96, 'hf': 80,  # fundo do logotipo
+                    'xh': 155, 'yh': 518, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
+                    'xfh': 155, 'yfh': 518, 'wfh': 359, 'hfh': 57,  # fundo do cabeçalho
+                }
+            ),
+
+
+        ]
+
+        arquivos = [(
+            '/home/leandro/Câmara/autografos/2021/846/aut_846.pdf',
+            '/home/leandro/Câmara/autografos/2021/aut_846__completo.pdf',
+            {}
+        ),
+        ]
+        for i in range(1, 15):
+            arquivos.append(
+                (
+                    f'/home/leandro/Câmara/autografos/2021/846/anexo/{i}.pdf',
+                    f'/home/leandro/Câmara/autografos/2021/846/anexo/{i}_out.pdf',
+                    coordenadas
+                ),
+            )
+
+        return arquivos
     def get_parametros_847(self):
 
         coordenadas = [
             (
-                29,
+                81,
                 {
-                    'x': 42, 'y': 478, 'w': 117, 'h': 90,  # logotipo
-                    'xf': 40, 'yf': 476, 'wf': 120, 'hf': 94,  # fundo do logotipo
-                    'xh': 165, 'yh': 521, 'wh': 350, 'hh': 49,  # cabeçalho horizontal
-                    'xfh': 165, 'yfh': 521, 'wfh': 450, 'hfh': 49,  # fundo do cabeçalho
+                    'x': 84, 'y': 476, 'w': 115, 'h': 90,  # logotipo
+                    'xf': 82, 'yf': 475, 'wf': 118, 'hf': 94,  # fundo do logotipo
+                    'xh': 207, 'yh': 511, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
+                    'xfh': 206, 'yfh': 507, 'wfh': 450, 'hfh': 62,  # fundo do cabeçalho
                 }
             ),
-
             (
-                21,
+                80,
                 {
-                    'x': 58, 'y': 482, 'w': 114, 'h': 90,  # logotipo
-                    'xf': 56, 'yf': 480, 'wf': 117, 'hf': 94,  # fundo do logotipo
-                    'xh': 179, 'yh': 514, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
-                    'xfh': 177, 'yfh': 513, 'wfh': 450, 'hfh': 62,  # fundo do cabeçalho
+                    'x': 42, 'y': 476, 'w': 115, 'h': 90,  # logotipo
+                    'xf': 40, 'yf': 475, 'wf': 118, 'hf': 94,  # fundo do logotipo
+                    'xh': 165, 'yh': 511, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
+                    'xfh': 165, 'yfh': 509, 'wfh': 450, 'hfh': 60,  # fundo do cabeçalho
                 }
             ),
-
             (
-                17,
+                72,
                 {
-                    'x': 57, 'y': 477, 'w': 117, 'h': 90,  # logotipo
-                    'xf': 55, 'yf': 475, 'wf': 122, 'hf': 94,  # fundo do logotipo
-                    'xh': 183, 'yh': 510, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
-                    'xfh': 181, 'yfh': 505, 'wfh': 450, 'hfh': 65,  # fundo do cabeçalho
+                    'x': 57, 'y': 481, 'w': 115, 'h': 90,  # logotipo
+                    'xf': 56, 'yf': 480, 'wf': 116, 'hf': 94,  # fundo do logotipo
+                    'xh': 177, 'yh': 515, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
+                    'xfh': 177, 'yfh': 515, 'wfh': 450, 'hfh': 57,  # fundo do cabeçalho
                 }
             ),
-
             (
-                16,
+                68,
                 {
-                    'x': 35, 'y': 477, 'w': 117, 'h': 90,  # logotipo
-                    'xf': 33, 'yf': 475, 'wf': 119, 'hf': 94,  # fundo do logotipo
-                    'xh': 161, 'yh': 510, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
-                    'xfh': 159, 'yfh': 505, 'wfh': 450, 'hfh': 65,  # fundo do cabeçalho
+                    'x': 58, 'y': 477, 'w': 117, 'h': 90,  # logotipo
+                    'xf': 57, 'yf': 475, 'wf': 119, 'hf': 94,  # fundo do logotipo
+                    'xh': 183, 'yh': 509, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
+                    'xfh': 183, 'yfh': 505, 'wfh': 450, 'hfh': 63,  # fundo do cabeçalho
                 }
             ),
-
+            
             (
-                14,
+                67,
+                {
+                    'x': 33, 'y': 477, 'w': 117, 'h': 90,  # logotipo
+                    'xf': 32, 'yf': 475, 'wf': 119, 'hf': 94,  # fundo do logotipo
+                    'xh': 157, 'yh': 509, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
+                    'xfh': 156, 'yfh': 502, 'wfh': 450, 'hfh': 67,  # fundo do cabeçalho
+                }
+            ),
+            
+            (
+                56,
+                {
+                    'x': 40, 'y': 477, 'w': 117, 'h': 90,  # logotipo
+                    'xf': 39, 'yf': 475, 'wf': 119, 'hf': 94,  # fundo do logotipo
+                    'xh': 167, 'yh': 509, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
+                    'xfh': 166, 'yfh': 502, 'wfh': 450, 'hfh': 67,  # fundo do cabeçalho
+                }
+            ),
+            
+            (
+                54,
                 {
                     'x': 57, 'y': 495, 'w': 92, 'h': 70,  # logotipo
                     'xf': 55, 'yf': 487, 'wf': 96, 'hf': 83,  # fundo do logotipo
@@ -85,13 +140,23 @@ class Command(BaseCommand):
                 }
             ),
 
+            
+            (
+                27,
+                {
+                    'x': 50, 'y': 478, 'w': 117, 'h': 90,  # logotipo
+                    'xf': 48, 'yf': 476, 'wf': 120, 'hf': 94,  # fundo do logotipo
+                    'xh': 175, 'yh': 521, 'wh': 350, 'hh': 49,  # cabeçalho horizontal
+                    'xfh': 175, 'yfh': 521, 'wfh': 450, 'hfh': 49,  # fundo do cabeçalho
+                }
+            ),
             (
                 0,
                 {
-                    'x': 57, 'y': 500, 'w': 92, 'h': 70,  # logotipo
-                    'xf': 55, 'yf': 492, 'wf': 96, 'hf': 83,  # fundo do logotipo
-                    'xh': 155, 'yh': 518, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
-                    'xfh': 155, 'yfh': 518, 'wfh': 359, 'hfh': 57,  # fundo do cabeçalho
+                    'x': 82, 'y': 477, 'w': 117, 'h': 90,  # logotipo
+                    'xf': 81, 'yf': 475, 'wf': 119, 'hf': 94,  # fundo do logotipo
+                    'xh': 207, 'yh': 509, 'wh': 359, 'hh': 57,  # cabeçalho horizontal
+                    'xfh': 206, 'yfh': 502, 'wfh': 450, 'hfh': 67,  # fundo do cabeçalho
                 }
             ),
 
@@ -103,11 +168,11 @@ class Command(BaseCommand):
             {}
         ),
         ]
-        for i in range(1, 2):
+        for i in range(1, 14):
             arquivos.append(
                 (
-                    f'/home/leandro/Câmara/autografos/2021/847/anexo/a{i}.pdf',
-                    f'/home/leandro/Câmara/autografos/2021/847/anexo/a{i}_out.pdf',
+                    f'/home/leandro/Câmara/autografos/2021/847/anexo/{i}.pdf',
+                    f'/home/leandro/Câmara/autografos/2021/847/anexo/{i}_out.pdf',
                     coordenadas
                 ),
             )
@@ -118,7 +183,7 @@ class Command(BaseCommand):
 
         coordenadas = [
             (
-                339,
+                332,
                 {
                     'x': 82, 'y': 477, 'w': 117, 'h': 90,  # logotipo
                     'xf': 81, 'yf': 475, 'wf': 119, 'hf': 94,  # fundo do logotipo
@@ -128,7 +193,7 @@ class Command(BaseCommand):
             ),
 
             (
-                255,
+                248,
                 {
                     'x': 41, 'y': 477, 'w': 117, 'h': 90,  # logotipo
                     'xf': 39, 'yf': 475, 'wf': 119, 'hf': 94,  # fundo do logotipo
@@ -155,47 +220,14 @@ class Command(BaseCommand):
             {}
         ),
         ]
-        for i in range(1, 2):
+        for i in range(1, 9):
             arquivos.append(
                 (
-                    f'/home/leandro/Câmara/autografos/2021/845/anexo/a{i}.pdf',
-                    f'/home/leandro/Câmara/autografos/2021/845/anexo/a{i}_out.pdf',
+                    f'/home/leandro/Câmara/autografos/2021/845/anexo/{i}.pdf',
+                    f'/home/leandro/Câmara/autografos/2021/845/anexo/{i}_out.pdf',
                     coordenadas
                 ),
             )
-
-        return arquivos
-
-    def get_parametros_846(self):
-
-        coordenadas = {
-            'x': 84, 'y': 479, 'w': 112, 'h': 80,  # logotipo
-            'xf': 79, 'yf': 474, 'wf': 122, 'hf': 95,  # fundo do logotipo
-            'xh': 205, 'yh': 521, 'wh': 362, 'hh': 48,  # cabeçalho horizontal
-            'xfh': 205, 'yfh': 521, 'wfh': 362, 'hfh': 48,  # fundo do cabeçalho
-        }
-        arquivos = [
-            [
-                '/home/leandro/Câmara/autografos/2021/846/846.pdf',
-                '/home/leandro/Câmara/autografos/2021/846.pdf',
-                {}
-            ],
-        ]
-        for i in range(1, 12):
-            arquivos.append(
-                [
-                    f'/home/leandro/Câmara/autografos/2021/846/anexo/a{i}.pdf',
-                    f'/home/leandro/Câmara/autografos/2021/846/anexo/a{i}_out.pdf',
-                    coordenadas
-                ],
-            )
-
-        arquivos[1][2] = {
-            'x': 35, 'y': 479, 'w': 112, 'h': 80,  # logotipo
-            'xf': 30, 'yf': 474, 'wf': 122, 'hf': 95,  # fundo do logotipo
-            'xh': 156, 'yh': 521, 'wh': 362, 'hh': 48,  # cabeçalho horizontal
-            'xfh': 156, 'yfh': 521, 'wfh': 410, 'hfh': 48  # fundo do cabeçalho
-        }
 
         return arquivos
 
@@ -203,16 +235,18 @@ class Command(BaseCommand):
 
         page_mask = None
         pl = None
-
+        count_page = 0
         def pagina_com_logotipo(coord, num_page):
-            print(num_page)
 
             nonlocal page_mask
             nonlocal pl
+            nonlocal count_page
+
+            print(count_page, num_page)
 
             v = None
             for p, va in coord:
-                if num_page >= p:
+                if count_page >= p:
                     v = va
                     break
 
@@ -227,11 +261,16 @@ class Command(BaseCommand):
             packet = io.BytesIO()
             try:
                 can = canvas.Canvas(
-                    packet, pagesize=landscape(A4))  # landscape(A4)
-
-                #can.setFillColorRGB(255, 255, 0, 1)
+                    packet, pagesize=portrait(A4))  # landscape(A4)
+                # can.setFillColorRGB(255, 255, 0, 1)
+                can.rotate(90)
+                can.translate(0, -595)
+                # can.setPageRotation(270)
                 can.setFillColorCMYK(
                     0, 0, 255 if partes_ou_completo == 'P' else 0, 0, 1)
+                """can.setStrokeColorCMYK(
+                    0, 0, 255 if partes_ou_completo == 'P' else 0, 0, 1)"""
+                #can.rect(0, 0, 842, 595, stroke=0, fill=1)
                 can.rect(v['xf'], v['yf'], v['wf'], v['hf'], stroke=0, fill=1)
                 can.drawImage(logo_camara, v['x'], v['y'], v['w'], v['h'])
 
@@ -261,8 +300,9 @@ class Command(BaseCommand):
 
         if partes_ou_completo == 'P':
             vo = ''
+            
             for i, o, coord in arquivos:
-
+                print(i)
                 ofile = PdfFileWriter()
                 vo = o
 
@@ -273,20 +313,24 @@ class Command(BaseCommand):
                     for num_page in range(ifile.getNumPages()):
                         xy.append(coord)
 
+                page_mask = None
                 pl = None
                 for num_page in range(ifile.getNumPages()):
 
-                    if num_page < 0:
+                    """if num_page < 0:
                         continue
 
-                    if num_page > 300:
-                        break
+                    if num_page > 0:
+                        break"""
 
                     if coord:
                         pl = pagina_com_logotipo(coord, num_page)
+                        count_page += 1
+                        
                     p = ifile.getPage(num_page)
-                    if coord:
+                    if coord:      
                         p.mergePage(pl.getPage(0))
+                        #p.mergeRotatedPage(pl.getPage(0), 90, False)
 
                     ofile.addPage(p)
 
@@ -304,10 +348,13 @@ class Command(BaseCommand):
 
                 ifile = PdfFileReader(open(i, "rb"))
 
+                page_mask = None
+                pl = None
                 for num_page in range(ifile.getNumPages()):
 
                     if coord:
                         pl = pagina_com_logotipo(coord, num_page)
+                        count_page += 1
 
                     p = ifile.getPage(num_page)
                     if coord:
