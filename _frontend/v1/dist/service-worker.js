28e320f04b665f1c76db0c851d2844b1cabc0bd4
LeandroJatai <leandroroberto.br@gmail.com>
2022-06-06 09:58:51 -0300

    ajustes no acesso rápido

--- _frontend/v1/dist/service-worker.js
+++ _frontend/v1/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/static/precache-manifest.c17ea8300e982f74de5846023c94b302.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/static/precache-manifest.e06aad77b2b0df094fe2579d9991391a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef */
 if (workbox) {
