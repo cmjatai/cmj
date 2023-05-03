09daea8e2242fd4252e46c43e3297e8f2fd8aab9
LeandroJatai <leandroroberto.br@gmail.com>
2023-05-03 16:47:15 -0300

    invert tree2

--- _frontend/v1/dist/service-worker.js
+++ _frontend/v1/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/static/precache-manifest.f0dd2a72f3be403b335c9f02cf689b10.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/static/precache-manifest.a0d52d189dc84a805c2b2a0bd78998e7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef */
 if (workbox) {
