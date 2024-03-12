e7168f3a1d4a31e4b7443a46f83a63dd1854d0a5
LeandroJatai <leandroroberto.br@gmail.com>
2024-03-12 10:15:53 -0300

    add refatoração de solr ao docker de produção

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo -e "\033[38;2;255;255;0;2m\033[1m====> StartPRD...\033[0m"
+
 /bin/bash wait-for-pg.sh "postgresql://cmj:cmj@cmjdb:5432/cmj"
 
 yes yes | python3 manage.py migrate
@@ -7,7 +9,7 @@ yes yes | python3 manage.py migrate
 ## SOLR
 USE_SOLR="${USE_SOLR:=False}"
 SOLR_URL="${SOLR_URL:=http://localhost:8983}"
-SOLR_COLLECTION="${SOLR_COLLECTION:=sapl}"
+SOLR_COLLECTIONS="${SOLR_COLLECTIONS:=portalcmj_cmj}"
 NUM_SHARDS=${NUM_SHARDS:=1}
 RF=${RF:=1}
 MAX_SHARDS_PER_NODE=${MAX_SHARDS_PER_NODE:=1}
@@ -18,7 +20,7 @@ if [ "${USE_SOLR-False}" == "True" ] || [ "${USE_SOLR-False}" == "true" ]; then
     echo "Solr configurations"
     echo "==================="
     echo "URL: $SOLR_URL"
-    echo "COLLECTION: $SOLR_COLLECTION"
+    echo "COLLECTION: $SOLR_COLLECTIONS"
     echo "NUM_SHARDS: $NUM_SHARDS"
     echo "REPLICATION FACTOR: $RF"
     echo "MAX SHARDS PER NODE: $MAX_SHARDS_PER_NODE"
@@ -38,7 +40,7 @@ if [ "${USE_SOLR-False}" == "True" ] || [ "${USE_SOLR-False}" == "true" ]; then
             echo "Assuming embedded ZooKeeper instalation..."
         fi
 
-        python3 solr_cli.py -u $SOLR_URL -c $SOLR_COLLECTION -s $NUM_SHARDS -rf $RF -ms $MAX_SHARDS_PER_NODE $ZK_EMBEDDED &
+        python3 solr_cli.py -u $SOLR_URL -c $SOLR_COLLECTIONS -s $NUM_SHARDS -rf $RF -ms $MAX_SHARDS_PER_NODE $ZK_EMBEDDED &
     else
         echo "Solr is offline, not possible to connect."
     fi
