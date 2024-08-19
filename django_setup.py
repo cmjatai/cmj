5d826ae2d3d11edf7b6dae1909b74bba19103e1e
LeandroJatai <leandroroberto.br@gmail.com>
2024-08-19 08:15:30 -0300

    add django_setup.py

---
+++ django_setup.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+import sys
+
+import django
+
+
+PROJECT_ROOT_DIR = Path(os.path.abspath(__file__)).parents[1]
+DJANGO_ROOT_DIR = PROJECT_ROOT_DIR / "cmj"
+
+
+def django_setup() -> None:
+    """
+    Allows to setup Django if it's not already running on a server. Should be called before any Django imports.
+    """
+    # Add the project base directory to the sys.path
+    sys.path.append(DJANGO_ROOT_DIR.as_posix())
+
+    # The DJANGO_SETTINGS_MODULE has to be set to allow us to access django
+    # imports
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'cmj.settings'
+
+    # This is for setting up django
+    django.setup()
