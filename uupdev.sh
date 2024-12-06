e5b55843481ff5240533f9e14a060f7dd41b206b
LeandroJatai <leandroroberto.br@gmail.com>
2024-12-06 07:59:48 -0300

    ajustes no gitignore

--- uupdev.sh
+++ uupdev.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+
+SSHFS_EXEC="$(ps -e -o comm)"
+
+if [[ ! $SSHFS_EXEC =~ "sshfs" ]]; then
+   echo "Chamando SSHFS"
+fi
+echo "Executando Compose DEV"
 sudo docker compose -f docker/docker-compose-dev.yaml up --build -d
