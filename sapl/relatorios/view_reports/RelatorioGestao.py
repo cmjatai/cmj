702588dd87a9b9c64a72c9b64b413a73b4dca1b5
LeandroJatai <leandroroberto.br@gmail.com>
2025-05-08 17:00:16 -0300

    feat: relatorio de gestão

--- sapl/relatorios/view_reports/RelatorioGestao.py
+++ sapl/relatorios/view_reports/RelatorioGestao.py
@@ -37,10 +37,10 @@ class View(RelatorioMixin, TemplateView):
         )
         context['html_body'] = self.get_html(
             [
+                self.get_producao_parlamentar(),
                 self.get_comissoes(),
                 self.get_normas(),
                 self.get_materias(),
-                self.get_producao_parlamentar(),
                 self.get_sessoes()
             ]
         )
@@ -64,9 +64,17 @@ class View(RelatorioMixin, TemplateView):
                         makeTocExtension(slugify=slugify_unicode), #TOC
                     ])
                 cols.append(f'<div class="{css_class}">{m}</div>')
-            row = f'<div class="row {"page-break" if i else ""}">{"".join(cols)}</div>'
+            row = f'<div class="row">{"".join(cols)}</div>'
             html.append(row)
         return ''.join(html)
+
+    def md2html(self, md_text):
+        """
+        Converte o texto markdown para html
+        """
+        return md.markdown(md_text, extensions=[
+            makeTocExtension(slugify=slugify_unicode), #TOC
+        ])
 
 
     def get_capa(self):
@@ -77,8 +85,9 @@ class View(RelatorioMixin, TemplateView):
         html0 = f'''
             <div class="capa">
                 Relatório Anual
+                de Gestão e
                 <br>
-                de Atividades Legislativas
+                Atividades Legislativas
                 <br>
                 <span class="capa-ano">
                 {self.ano}
@@ -124,7 +133,7 @@ class View(RelatorioMixin, TemplateView):
         """
         Retorna a página de comissões do relatório
         """
-        mark0 = [f'# Comissões']
+        mark0 = [f'# 6. Comissões']
 
         participacoes = Participacao.objects.filter(
             composicao__periodo__data_inicio__year__lte=self.ano,
@@ -146,41 +155,47 @@ class View(RelatorioMixin, TemplateView):
 
             comissao.append(f'* {participacao.cargo.nome.upper()}: **{participacao.parlamentar.nome_parlamentar}**')
 
-        r = [('\n'.join(mark0), 'container-docs1 col-md-12', 'markdown'),]
+        r = [('\n'.join(mark0), 'col-md-12 new-page', 'markdown'),]
 
         length_comissoes = len(comissoes)
-        column1 = length_comissoes // 2
-        column2 = length_comissoes - column1
-        if column1 == 0:
-            column1 = 1
-        if column2 == 0:
-            column2 = 1
+        row1_from_column2 = length_comissoes // 2
+        row2_from_column2 = length_comissoes - row1_from_column2
 
+        if row1_from_column2 % 2 == 1:
+            row1_from_column2 += 1
+            row2_from_column2 = row1_from_column2 - 2
+
+        row3_from_column2 = length_comissoes - row1_from_column2 - row2_from_column2
 
         mark1 = []
         mark2 = []
+        mark3 = []
         for i, comissao in enumerate(comissoes):
-            if i < column1:
+            if i < row1_from_column2:
                 mark1.append('')
                 mark1.append('\n'.join(comissao))
-            else:
+            elif i < row1_from_column2 + row2_from_column2:
                 mark2.append('')
                 mark2.append('\n'.join(comissao))
+            else:
+                mark3.append('')
+                mark3.append('\n'.join(comissao))
 
-        r.append(('\n'.join(mark1), 'container-docs1 col-md-6 columns2', 'markdown'))
-        r.append(('\n'.join(mark2), 'container-docs1 col-md-6 columns2', 'markdown'))
+        r.append(('\n'.join(mark1), 'col-md-6 columns2', 'markdown'))
+        r.append(('\n'.join(mark2), 'col-md-6 columns2', 'markdown'))
 
+        if mark3:
+            r.append(('\n'.join(mark3), 'col-md-12', 'markdown'))
 
         return r
 
     def get_normas(self):
 
-        mark = [f'# Atos Normativos/Legislativos de {self.ano}']
+        mark = [f'# 7. Atos Normativos/Legislativos de {self.ano}']
         tipos = TipoNormaJuridica.objects.filter(
             normajuridica_set__data__year=self.ano
         ).annotate(Count('normajuridica_set')).order_by('relevancia')
         for t in tipos:
-            mark.append('')
             mark.append(f'* [{t.descricao} ({t.normajuridica_set__count})]'
                         f'(http://www.jatai.go.leg.br/pesquisar/norma?tipo_i={t.pk}&ano_i={self.ano})')
 
@@ -190,28 +205,27 @@ class View(RelatorioMixin, TemplateView):
         assuntos = AssuntoNorma.objects.filter(
             normajuridica__data__year=self.ano
         ).annotate(Count('normajuridica')).order_by('assunto')
-        for a in assuntos:
-            mark2.append('')
-            mark2.append(
-                f'* [{a.assunto} ({a.normajuridica__count})]'
+
+        mark21 = []
+        for i, a in enumerate(assuntos):
+            text = f'* [{a.assunto} ({a.normajuridica__count})]' \
                 f'(http://www.jatai.go.leg.br/pesquisar/norma?assuntos_is={a.pk}&ano_i={self.ano})'
-            )
 
-
+            mark21.append(text)
         return (
-            ('\n'.join(mark), 'container-docs1 col-md-12', 'markdown'),
-            ('\n'.join(mark2), 'container-docs2 col-md-12', 'markdown'),
+            ('\n'.join(mark), 'col-12 new-page', 'markdown'),
+            ('\n'.join(mark2), 'col-12', 'markdown'),
+            ('\n'.join(mark21), 'col-12 col-md-6 columns2', 'markdown'),
         )
 
     def get_materias(self):
 
-        mark = [f'# Matérias Legislativas no ano de {self.ano}.']
+        mark = [f'# 8. Matérias Legislativas no ano de {self.ano}']
         tipos =TipoMateriaLegislativa.objects.filter(
             materialegislativa__ano=self.ano,
         ).annotate(Count('materialegislativa')).order_by('sequencia_regimental')
 
         for t in tipos:
-            mark.append('')
             mark.append(f'* [{t.descricao} ({t.materialegislativa__count})]'
                         f'(http://www.jatai.go.leg.br/pesquisar/materia?tipo_i={t.pk}&ano_i={self.ano})')
 
@@ -224,31 +238,34 @@ class View(RelatorioMixin, TemplateView):
         for i, a in enumerate(assuntos):
             if not i:
                 mark2.append('')
-                mark2.append(f'#### Assuntos dos Requerimentos de {self.ano}')
-            mark2.append('')
+                mark2.append(f'### Assuntos dos Requerimentos de {self.ano}')
             mark2.append(
                 f'* [{a.assunto} ({a.materialegislativa__count})]'
                 f'(http://www.jatai.go.leg.br/pesquisar/materia?assuntos_is={a.pk}&ano_i={self.ano})'
             )
 
         return (
-            ('\n'.join(mark), 'container-docs1 col-md-12', 'markdown'),
-            ('\n'.join(mark2), 'container-docs2 col-md-12', 'markdown'),
+            ('\n'.join(mark), 'col-md-12 new-page', 'markdown'),
+            ('\n'.join(mark2[:2]), 'col-md-12', 'markdown'),
+            ('\n'.join(mark2[2:]), 'col-md-12 columns2', 'markdown'),
         )
 
     def get_producao_parlamentar(self):
         """
         Retorna a página de produção parlamentar por parlamentar
         """
-        mark = [f'# Produção Legislativa dos Parlamentares de {self.ano}']
-        mark2 = []
+        mark0 = [f'# 5. Produção Legislativa em {self.ano}']
+        marks = []
+
+        cols = []
         for p in self.parlamentares:
             autor = p.autor.first()
             if not autor:
                 continue
 
-            mark2.append('')
-            mark2.append(f'### {p.nome_parlamentar}')
+            col = []
+            col.append('')
+            col.append(f'### {p.nome_parlamentar}')
 
             tipos_de_materia_do_parlamentar = TipoMateriaLegislativa.objects.filter(
                 materialegislativa__ano=self.ano,
@@ -256,28 +273,23 @@ class View(RelatorioMixin, TemplateView):
                 materialegislativa__autoria__autor=autor,
             ).annotate(Count('materialegislativa'))
             for t in tipos_de_materia_do_parlamentar:
-                mark2.append(f'* [{t.descricao} ({t.materialegislativa__count})]'
+                col.append(f'* [{t.descricao} ({t.materialegislativa__count})]'
                             f'(http://www.jatai.go.leg.br/pesquisar/materia?tipo_i={t.pk}&ano_i={self.ano}&autoria_is={autor.pk})')
 
-            """mark.append('')
-            mark.append(f'#### Assuntos Requeridos pelo Parlamentar')
+            html_col = '<div class="col-md-6 inside-columns">{}</div>'.format(
+                self.md2html('\n'.join(col))
+            )
 
-            assuntos = AssuntoMateria.objects.filter(
-                materialegislativa__ano=self.ano,
-                materialegislativa__tipo_id=3,
-                materialegislativa__autoria__autor=autor,
-            ).annotate(Count('materialegislativa')).order_by('assunto')
-            for a in assuntos:
-                mark.append('')
-                mark.append(
-                    f'* [{a.assunto} ({a.materialegislativa__count})]'
-                    f'(http://www.jatai.go.leg.br/pesquisar/materia?assuntos_is={a.pk}&ano_i={self.ano}&autoria_is={autor.pk})'
-                )"""
+            cols.append(html_col)
 
-        return (
-            ('\n'.join(mark), 'container-docs1 col-md-12', 'markdown'),
-            ('\n'.join(mark2), 'container-docs1 columns2 col-md-12', 'markdown'),
-        )
+            if len(cols) == 2:
+                marks.append((''.join(cols), 'col-md-6 columns2', 'html'))
+                cols = []
+
+        marks.insert(0, ('\n'.join(mark0), 'col-md-12', 'markdown'))
+
+        return marks
+
 
     def get_sessoes(self):
         """
@@ -287,13 +299,13 @@ class View(RelatorioMixin, TemplateView):
             sessaoplenaria__data_inicio__year=self.ano,
         ).annotate(Count('sessaoplenaria'))
 
-        mark = [f'# Sessões Plenárias de {self.ano}']
+        mark = [f'# 9. Sessões Plenárias de {self.ano}']
+
         for t in tipos_de_sessao:
-            mark.append('')
             mark.append(
                 f'* [{t.nome} ({t.sessaoplenaria__count})]'
                 f'(http://www.jatai.go.leg.br/sessao/pesquisar-sessao?tipo={t.pk}&data_inicio__year={self.ano})')
 
         return (
-            ('\n'.join(mark), 'container-docs1 col-md-12', 'markdown'),
+            ('\n'.join(mark), 'col-md-12', 'markdown'),
         )
