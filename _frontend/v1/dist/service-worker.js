3be10f10279fb88d32aecd026f8a0b5dd2065da4
LeandroJatai <leandroroberto.br@gmail.com>
2025-06-18 16:48:54 -0300

    rebuild frontend

--- _frontend/v1/dist/service-worker.js
+++ _frontend/v1/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/static/precache-manifest.85e3a7c387393ae796ba26806222b17c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/static/precache-manifest.e963baee4ecdaf3ad7e3631f9edd21fa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef */
 if (workbox) {
