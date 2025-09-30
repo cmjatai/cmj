d1941680243adab880e8ff2e055fe8f56e51a1bf
LeandroJatai <leandroroberto.br@gmail.com>
2025-09-30 17:03:19 -0300

    feat: estrutura de arquivos frontend para painelset

---
+++ _frontend/v1/src/modules/painelset/admin/PainelSetAdmin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="painelset-admin">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'painelset-admin',
+  data () {
+    return {
+    }
+  },
+
+  mounted: function () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
