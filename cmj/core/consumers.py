4b901635e9a90c2a33dc4db38f63c757ff29b7af
LeandroJatai <leandroroberto.br@gmail.com>
2025-10-13 18:20:22 -0300

    ajustes no sync

--- cmj/core/consumers.py
+++ cmj/core/consumers.py
@@ -117,75 +117,84 @@ class SyncRefreshConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         jdata = json.loads(text_data)
 
-        type_msg = jdata.get('type', '')
+        try:
 
-        if type_msg == 'ping':
-            timestamp = jdata.get('timestamp_client', '')
-            ping_now = jdata.get('ping_now', '')
+            type_msg = jdata.get('type', '')
+
+            if type_msg == 'ping':
+                timestamp = jdata.get('timestamp_client', '')
+                ping_now = jdata.get('ping_now', '')
+                await self.send(text_data=json.dumps({
+                    'type': 'pong',
+                    'message': {
+                        'timestamp_server': time.time(), # timezone.now().timestamp(),
+                        'timestamp_client': timestamp,
+                        'ping_now': ping_now
+                    }
+                }))
+                return
+
+            if type_msg == 'command':
+                command = jdata.get('command', '')
+                app = jdata.get('app', '')
+                model = jdata.get('model', '')
+                params = jdata.get('params', {})
+                user = self.scope.get('user')
+
+                if app == 'painelset' and model == 'cronometro':
+
+                    if not command:
+                        await self.send(text_data=json.dumps({
+                            'type': 'command_result',
+                            'command': command,
+                            'error': 'Comando não especificado'
+                        }))
+                        return
+
+                    if not user.has_perm('painelset.change_cronometro'):
+                        await self.send(text_data=json.dumps({
+                            'type': 'command_result',
+                            'command': command,
+                            'error': 'Usuário não autenticado'
+                        }))
+                        return
+
+                    if command in ['start', 'pause', 'resume', 'stop', 'add_time']:
+                        cronometro_id = params.get('id')
+                        seconds = params.get('seconds', 0)
+                        if not cronometro_id:
+                            await self.send(text_data=json.dumps({
+                                'type': 'command_result',
+                                'command': command,
+                                'error': 'ID do cronômetro não especificado'
+                            }))
+                            return
+
+                        command_manager = getattr(self.cronometro_manager, f'{command}_cronometro', None)
+                        if not command_manager:
+                            await self.send(text_data=json.dumps({
+                                'type': 'command_result',
+                                'command': command,
+                                'error': f'Comando inválido: {command}'
+                            }))
+                            return
+
+                        result = await database_sync_to_async(
+                            command_manager
+                        )(cronometro_id, seconds=seconds if command == 'add_time' else None)
+
+                        #await self.send(text_data=json.dumps({
+                        #    'type': 'command_result',
+                        #    'command': command,
+                        #    'result': result
+                        #}))
+        except Exception as e:
+            logger.error(f"Erro ao processar mensagem no SyncRefreshConsumer: {e}")
             await self.send(text_data=json.dumps({
-                'type': 'pong',
-                'message': {
-                    'timestamp_server': time.time(), # timezone.now().timestamp(),
-                    'timestamp_client': timestamp,
-                    'ping_now': ping_now
-                }
+                'type': 'command_result',
+                'command': jdata.get('command', ''),
+                'error': str(e)
             }))
-            return
-
-        if type_msg == 'command':
-            command = jdata.get('command', '')
-            app = jdata.get('app', '')
-            model = jdata.get('model', '')
-            params = jdata.get('params', {})
-            user = self.scope.get('user')
-
-            if app == 'painelset' and model == 'cronometro':
-
-                if not command:
-                    await self.send(text_data=json.dumps({
-                        'type': 'command_result',
-                        'command': command,
-                        'error': 'Comando não especificado'
-                    }))
-                    return
-
-                if not user.has_perm('painelset.change_cronometro'):
-                    await self.send(text_data=json.dumps({
-                        'type': 'command_result',
-                        'command': command,
-                        'error': 'Usuário não autenticado'
-                    }))
-                    return
-
-                if command in ['start', 'pause', 'resume', 'stop', 'add_time']:
-                    cronometro_id = params.get('id')
-                    seconds = params.get('seconds', 0)
-                    if not cronometro_id:
-                        await self.send(text_data=json.dumps({
-                            'type': 'command_result',
-                            'command': command,
-                            'error': 'ID do cronômetro não especificado'
-                        }))
-                        return
-
-                    command_manager = getattr(self.cronometro_manager, f'{command}_cronometro', None)
-                    if not command_manager:
-                        await self.send(text_data=json.dumps({
-                            'type': 'command_result',
-                            'command': command,
-                            'error': f'Comando inválido: {command}'
-                        }))
-                        return
-
-                    result = await database_sync_to_async(
-                        command_manager
-                    )(cronometro_id, seconds=seconds if command == 'add_time' else None)
-
-                    #await self.send(text_data=json.dumps({
-                    #    'type': 'command_result',
-                    #    'command': command,
-                    #    'result': result
-                    #}))
 
 
     async def command_result(self, event):
@@ -199,29 +208,38 @@ class SyncRefreshConsumer(AsyncWebsocketConsumer):
     # Receive message from room group
     async def sync_refresh_message(self, event):
 
-        message = event['message']
-        app = message.get('app')
-        model = message.get('model')
+        try:
 
-        u = self.scope.get('user')
+            message = event['message']
+            app = message.get('app')
+            model = message.get('model')
 
-        key = f"{app}:{model}"
-        perms_publicas = self.portalcmj_rpp.get(key, set())
+            u = self.scope.get('user')
 
-        perm_detail = f"{app}.detail_{model}"
-        perm_view = f"{app}.view_{model}"
+            key = f"{app}:{model}"
+            perms_publicas = self.portalcmj_rpp.get(key, set())
 
-        if u.is_anonymous and not perms_publicas:
-            detail_perm = False
-        elif u.is_anonymous:
-            detail_perm = perm_detail in perms_publicas or perm_view in perms_publicas
-        elif not u.is_superuser:
-            detail_perm = u.has_perm(perm_detail) or u.has_perm(perm_view)
-        else:
-            detail_perm = True
+            perm_detail = f"{app}.detail_{model}"
+            perm_view = f"{app}.view_{model}"
 
-        if not detail_perm:
-            del message['instance']
+            if u.is_anonymous and not perms_publicas:
+                detail_perm = False
+            elif u.is_anonymous:
+                detail_perm = perm_detail in perms_publicas or perm_view in perms_publicas
+            elif not u.is_superuser:
+                detail_perm = u.has_perm(perm_detail) or u.has_perm(perm_view)
+            else:
+                detail_perm = True
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps(event))
+            if not detail_perm:
+                del message['instance']
+
+            # Send message to WebSocket
+            await self.send(text_data=json.dumps(event))
+            
+        except Exception as e:
+            logger.error(f"Erro ao processar sync_refresh_message no SyncRefreshConsumer: {e}")
+            await self.send(text_data=json.dumps({
+                'type': 'error',
+                'error': str(e)
+            }))
