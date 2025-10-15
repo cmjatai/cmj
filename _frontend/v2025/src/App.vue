90103b30dfc9e410a9ec60ec1288be06eef9b681
LeandroJatai <leandroroberto.br@gmail.com>
2025-10-15 14:03:51 -0300

    custom btn de color mode

--- _frontend/v2025/src/App.vue
+++ _frontend/v2025/src/App.vue
@@ -1,19 +1,7 @@
 <template>
   <slot />
   <DisplayMessage />
-  syncStore.state: {{ syncStore.data_cache.painelset_cronometro }} <br>
 </template>
 <script setup>
 import DisplayMessage from '~@/modules/messages/components/DisplayMessage'
-import { useSyncStore } from '~@/stores/SyncStore'
-
-const syncStore = useSyncStore()
-syncStore.initialize()
-syncStore.registerModels(
-  'painelset', [
-    'evento',
-    'individuo',
-    'cronometro'
-  ]
-)
 </script>
