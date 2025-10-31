e30d502a24ab3a646bf0d30105091023b0da9632
LeandroJatai <leandroroberto.br@gmail.com>
2025-10-31 12:55:18 -0300

    feat: desativa tema outubro rosa no frontend

--- _frontend/v2018/dist/v2018/service-worker.js
+++ _frontend/v2018/dist/v2018/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/static/v2018/precache-manifest.12f3642950bb97a2cb56e706e36b419b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/static/v2018/precache-manifest.33f6ed5adaf33a83ac5a9e7541a4e57b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef */
 if (workbox) {
