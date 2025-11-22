c50b4c8aba53a69015a68aa770dad70955232779
LeandroJatai <leandroroberto.br@gmail.com>
2025-11-22 13:08:08 -0300

    remove fetch do sq

--- _frontend/v2025/src/service-worker.js
+++ _frontend/v2025/src/service-worker.js
@@ -1,18 +1,12 @@
-// Um Service Worker que não faz nada, apenas permite a instalação
-
 import { precacheAndRoute } from 'workbox-precaching'
 
 precacheAndRoute(self.__WB_MANIFEST)
 
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
+  self.skipWaiting()
+})
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
+  event.waitUntil(self.clients.claim())
+})
 
-self.addEventListener('fetch', (event) => {
-  // Não faz nada, deixa a requisição passar direto para a rede
-  return;
-});
