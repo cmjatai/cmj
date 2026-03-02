caccf9fc2e7c60900c15c99d3af47549806222fd
LeandroJatai <leandroroberto.br@gmail.com>
2026-03-02 10:20:56 -0300

    feat: impl parcial de busca por prestação de conta

--- cmj/api/forms.py
+++ cmj/api/forms.py
@@ -1,3 +1,9 @@
+from cmj.loa.models import PrestacaoContaRegistro
+from drfautoapi.drfautoapi import ApiFilterSetMixin
 
+class PrestacaoContaRegistroFilterSet(ApiFilterSetMixin):
+    class Meta(ApiFilterSetMixin.Meta):
+        model = PrestacaoContaRegistro
 
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
