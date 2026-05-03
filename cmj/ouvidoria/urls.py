85e8d91ada29957056d31906e3a77eb8a6ae0239
LeandroJatai <leandroroberto.br@gmail.com>
2026-05-03 14:58:46 -0300

    altera padrão de url ouvidoria desativada

--- cmj/ouvidoria/urls.py
+++ cmj/ouvidoria/urls.py
@@ -18,57 +18,57 @@ app_name = AppConfig.name
 
 urlpatterns_ouvidoria = [
     re_path(
-        r"^fale-conosco/ouvidoria/denuncia-anonima/?$",
+        r"^fale-conosco/ouvidoria_disabled/denuncia-anonima/?$",
         DenunciaAnonimaFormView.as_view(),
         name="denuncia_form",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria/solicitacao/(?P<pk>[0-9]+)/?$",
+        r"^fale-conosco/ouvidoria_disabled/solicitacao/(?P<pk>[0-9]+)/?$",
         SolicitacaoDetailView.as_view(),
         name="solicitacao_detail",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria/solicitacao/minhas/?$",
+        r"^fale-conosco/ouvidoria_disabled/solicitacao/minhas/?$",
         SolicitacaoListView.as_view(),
         name="solicitacao_list",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria/solicitacao/manage/?$",
+        r"^fale-conosco/ouvidoria_disabled/solicitacao/manage/?$",
         SolicitacaoManageListView.as_view(),
         name="solicitacao_manage_list",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria/solicitacao/registrar/?$",
+        r"^fale-conosco/ouvidoria_disabled/solicitacao/registrar/?$",
         SolicitacaoFormView.as_view(),
         name="solicitacao_create",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria/solicitacao/(?P<pk>[0-9]+)/interact$",
+        r"^fale-conosco/ouvidoria_disabled/solicitacao/(?P<pk>[0-9]+)/interact$",
         SolicitacaoInteractionView.as_view(),
         name="solicitacao_interact",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria/solicitacao/P(?P<hash>[0-9A-Fa-f]+)/(?P<pk>\d+)$",
+        r"^fale-conosco/ouvidoria_disabled/solicitacao/P(?P<hash>[0-9A-Fa-f]+)/(?P<pk>\d+)$",
         SolicitacaoInteractionView.as_view(),
         name="solicitacao_interact_hash",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria/solicitacao/(?P<pk>[0-9]+)/mensagem",
+        r"^fale-conosco/ouvidoria_disabled/solicitacao/(?P<pk>[0-9]+)/mensagem",
         SolicitacaoMensagemRedirect.as_view(),
         name="mensagemsolicitacao_detail",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria/mensagem/(?P<pk>[0-9]+)/anexo",
+        r"^fale-conosco/ouvidoria_disabled/mensagem/(?P<pk>[0-9]+)/anexo",
         SolicitacaoMensagemAnexoView.as_view(),
         name="anexo_mensagem_view",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria/mensagem/P(?P<hash>[0-9A-Fa-f]+)/(?P<pk>\d+)/anexo",
+        r"^fale-conosco/ouvidoria_disabled/mensagem/P(?P<hash>[0-9A-Fa-f]+)/(?P<pk>\d+)/anexo",
         SolicitacaoMensagemAnexoView.as_view(),
         name="anexo_mensagem_view_hash",
     ),
     re_path(
-        r"^fale-conosco/ouvidoria",
+        r"^fale-conosco/ouvidoria_disabled",
         OuvidoriaPaginaInicialView.as_view(),
         name="ouvidoria_pagina_inicial",
     ),
